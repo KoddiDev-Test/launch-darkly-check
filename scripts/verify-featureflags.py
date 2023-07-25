dc19b2561ddaf26965bff1dc52bbd3fd295eed34
Davis Shriver <135754067+Davis-Shriver@users.noreply.github.com>
2023-07-25 14:01:16 -0500

    Update verify-featureflags.py
    Refactored code

--- scripts/verify-featureflags.py
+++ scripts/verify-featureflags.py
@@ -1,28 +1,41 @@
-# Compare FF for specific environments
 import os
 import requests
 
-# Get FF for specific env
-PROJECT_KEY = os.getenv("PROJECT_KEY")
-ENV1 = os.getenv("ENV1")
-ENV2 = os.getenv("ENV2")
-API_KEY = os.getenv("API_KEY")
-URL = os.getenv("URL")
+def main():
+    # Fetch necessary environment variables
+    project_key = os.getenv("PROJECT_KEY")
+    env1 = os.getenv("ENV1")
+    env2 = os.getenv("ENV2")
+    api_key = os.getenv("API_KEY")
+    url = os.getenv("URL")
 
-headers = {"Authorization": API_KEY}
+    headers = {"Authorization": api_key}
 
-response = requests.get(URL + PROJECT_KEY, headers=headers)
-data = response.json()
+    try:
+        response = requests.get(url + project_key, headers=headers)
+        response.raise_for_status()
+        data = response.json()
 
-# Iterate through data to find the flag
-failedItems = []
-for item in data["items"]:
-    isMatch = item["environments"][ENV1]["on"] == item["environments"][ENV2]["on"]
-    if isMatch == False:
-         print("Flag failed to match: ", item["name"], " (Staging: ", item["environments"]["test"]["on"], "| Production: ", item["environments"]["production"]["on"],")")
-         failedItems.append(item["name"])
-# Check if anything failed
-if len(failedItems) != 0:
-    exit(1)
-else:
-    print(ENV1 + " and " + ENV2 + " flags match!")
+        # Iterate through data to find the flag
+        failed_items = []
+        for item in data["items"]:
+            env1_flag = item["environments"][env1]["on"]
+            env2_flag = item["environments"][env2]["on"]
+            is_match = (env1_flag == env2_flag)
+            if not is_match:
+                print(f"Flag failed to match: {item['name']} ({env1}: {env1_flag} | {env2}: {env2_flag})")
+                failed_items.append(item["name"])
+
+        # Check if anything failed
+        if failed_items:
+            print("Some flags failed to match!")
+            exit(1)
+        else:
+            print(f"{env1} and {env2} flags match!")
+
+    except requests.exceptions.RequestException as e:
+        print("Error occurred during the API request:", e)
+        exit(1)
+
+if __name__ == "__main__":
+    main()
